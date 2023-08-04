89c6822132a151a5d0178a49a96b7d3d16e61717
Jay Sim <jay.sim@vshn.net>
2023-08-04 11:15:13 -0700

    Initial Commit

---
+++ cli.py
@@ -0,0 +1,69 @@
+#from functions import get_todos, write_todos
+import functions
+import time
+
+now = time.strftime("%b %d, %Y %H:%M:%S")
+print("It is", now)
+
+while True:
+    user_action = input("Add or Show, edit, complete or exit? ")
+    user_action = user_action.strip()
+
+    if user_action.startswith("add"):
+        todo = user_action[4:]
+
+        todos = functions.get_todos()
+
+        todos.append(todo.title() + '\n')
+
+        functions.write_todos(todos)
+
+    elif user_action.startswith("show"):
+
+        todos = functions.get_todos()
+
+        for index, item in enumerate(todos):
+            item = item.strip('\n')
+            row = f"{index + 1}-{item}"
+            print(row)
+
+    elif user_action.startswith("edit"):
+        try:
+            number = int(user_action[5:])
+            number = number - 1
+
+            todos = functions.get_todos()
+
+            new_todo = input("Enter a new todo: ")
+            todos[number] = new_todo + '\n'
+
+            functions.write_todos(todos)
+
+        except ValueError:
+            print("Your Command Is Not Valid.")
+            continue
+
+    elif user_action.startswith("complete"):
+        try:
+            number = int(user_action[9:])
+
+            todos = functions.get_todos()
+
+            index = number - 1
+            todo_to_remove = todos[index].strip('\n')
+            todos.pop(index)
+
+            functions.write_todos(todos)
+
+            message = "\n" + f"'{todo_to_remove}' was removed" + "\n"
+            print(message)
+        except ValueError and IndexError:
+            print("Your Command Is Not Valid.")
+            continue
+
+    elif 'exit' in user_action:
+        break
+
+    else:
+        print(" Command is not valid!!! \n Please Enter again")
+print("Okay!")
