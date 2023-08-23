c6ba0fd7a4a3d07c3e856201ed5ed4efbbd35d77
jay sim <youthought2020@gmail.com>
2023-08-22 20:55:06 -0700

    Finish bonus18 execerise

--- Bonus/Section18CodingExercise.py
+++ Bonus/Section18CodingExercise.py
@@ -11,16 +11,22 @@ input2 = sg.Input(key="inches")
 compress_button = sg.Button("Convert")
 output_label = sg.Text(key="output", text_color='red')
 
+exit_button = sg.Exit('Exit')
+
 window = sg.Window('Convertor',
                    layout=[[label1, input1],
                            [label2, input2],
-                            [compress_button,output_label]])
+                            [compress_button, exit_button, output_label]])
 
 while True:
     event, values = window.read()
-    input1 = float(values["feet"])
-    input2 = float(values["inches"])
-    result = feet_to_inches(input1, input2)
-    window["output"].update(value=f"{result}")
+    match event:
+        case "Convert":
+            input1 = float(values["feet"])
+            input2 = float(values["inches"])
+            result = feet_to_inches(input1, input2)
+            window["output"].update(value=f"{result}")
+        case "Exit":
+            break
 
 window.close()
